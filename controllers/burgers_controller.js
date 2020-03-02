ebcbbdee4f1247658bd21f0db874c138a19b6fe6
Matthew Wester <MatthewWester@live.com>
2020-03-01 22:34:59 -0700

    Somehow got buttons working

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,42 +5,55 @@ const router = express.Router();
 const burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-    burger.selectAll(function(data) {
-      const hbsObject = {
-        burgers: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+  burger.all(function(data) {
+    const hbsObject = {
+      burgers: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
   });
+});
   
-  router.post("/api/burgers", function(req, res) {
-      console.log("The post is firing");
-    burger.create(["burger_name"], [req.body.name], function(result) {
-      console.log("This is what happens");
-      res.json({ id: result.insertId });
-    });
+router.post("/api/burgers", function(req, res) {
+  burger.create([
+    "name", "devoured"
+  ], [
+    req.body.name, req.body.devoured
+  ], function(result) {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
   });
+});
   
-  router.put("/api/burgers/:id", function(req, res) {
-    const condition = "id = " + req.params.id;
-  
-    console.log("devoured", condition);
-  
-    burger.updateOne(
-      {
-        devoured: req.body.devoured
-      },
-      condition,
-      function(result) {
-        if (result.changedRows == 0) {
-          return res.status(404).end();
-        } else {
-        res.status(200).end();
-        }
-      }
-    );
+router.put("/api/burgers/:id", function(req, res) {
+  const condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
   });
+});
+
+router.delete("/api/burgers/:id", function(req, res) {
+  const condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
   
-  module.exports = router;
+module.exports = router;
   
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO burgers (burger_name) VALUES ('Yummy Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Bacon Burger');
-INSERT INTO burgers (burger_name) VALUES ('Double Bison Burger');
+INSERT INTO burgers (name) VALUES ('Yummy Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Burger');
+INSERT INTO burgers (name) VALUES ('Double Bison Burger');
